aadccdf0c63de8252ae450464195daf6882c23ef
James Skemp <strivinglife@gmail.com>
2015-10-11 10:01:03 -0500

    Added assets we'll be using and rendering a star. Part 2 complete.

--- Official-Making-A-Game/src/game.js
+++ Official-Making-A-Game/src/game.js
@@ -4,11 +4,14 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
 });
 
 function preload() {
-
+	game.load.image('sky', 'assets/sky.png');
+	game.load.image('platform', 'assets/platform.png');
+	game.load.image('star', 'assets/star.png');
+	game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 }
 
 function create() {
-
+	game.add.sprite(0, 0, 'star');
 }
 
 function update() {
