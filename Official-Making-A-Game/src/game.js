ceadaffaebce4d05c44a9297d61791c5503c8bd9
James Skemp <strivinglife@gmail.com>
2015-10-11 09:48:22 -0500

    Part 1 of official tutorial.

---
+++ Official-Making-A-Game/src/game.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="../lib/phaser.min.js" />
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
+	preload: preload, create: create, update: update
+});
+
+function preload() {
+
+}
+
+function create() {
+
+}
+
+function update() {
+
+}
