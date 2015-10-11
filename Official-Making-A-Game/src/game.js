23ade414b72ec2fd2218117e925ae14995bbde59
James Skemp <strivinglife@gmail.com>
2015-10-11 10:08:57 -0500

    Finished part 3, which added background, ground, and platforms.

--- Official-Making-A-Game/src/game.js
+++ Official-Making-A-Game/src/game.js
@@ -10,10 +10,38 @@ function preload() {
 	game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 }
 
+var platforms;
+
 function create() {
-	game.add.sprite(0, 0, 'star');
+	// Enable arcade physics.
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+
+	// Background image.
+	game.add.sprite(0, 0, 'sky');
+
+	// Includes ground and the ledges.
+	platforms = game.add.group();
+
+	// Enable physics for all objects in this group.
+	platforms.enableBody = true;
+
+	// Create the ground.
+	var ground = platforms.create(0, game.world.height - 64, 'platform');
+
+	// Scale the image to fit the width of the game.
+	ground.scale.setTo(2, 2);
+
+	// Ground is solid and doesn't move.
+	ground.body.immovable = true;
+
+	// Create our first platform.
+	var ledge = platforms.create(400, 400, 'platform');
+	ledge.body.immovable = true;
+
+	// Create the second platform.
+	ledge = platforms.create(-150, 250, 'platform');
+	ledge.body.immovable = true;
 }
 
 function update() {
-
 }
