ac3bb1ad57c232aa0509881b58857020a9d46421
James Skemp <strivinglife@gmail.com>
2015-10-12 21:44:24 -0500

    Started working on the fifth coding tip demo.

---
+++ Coding-Tip-5-Car/README.md
@@ -0,0 +1,3 @@
+# Coding Tip 5: Car in Grid
+
+The following is from the official site's [Phaser Coding Tips 5: How to move like Pacman Tutorial](http://phaser.io/tutorials/coding-tips-005).
---
+++ Coding-Tip-5-Car/src/game.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="../lib/phaser-2.4.3.js" />
+var game = new Phaser.Game(640, 480, Phaser.AUTO);
+
+game.state.add('play', {
+	init: function() {
+	},
+
+	preload: function () {
+	},
+
+	create: function () {
+	},
+
+	update: function () {
+	},
+
+	render: function () {
+	}
+});
+
+game.state.start('play');
