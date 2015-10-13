639c5aedf1306522618de3d97329ef720c163ea2
James Skemp <strivinglife@gmail.com>
2015-10-12 22:08:06 -0500

    WIP. Now displays the map and car on screen.

--- Coding-Tip-5-Car/src/game.js
+++ Coding-Tip-5-Car/src/game.js
@@ -3,6 +3,7 @@ var game = new Phaser.Game(640, 480, Phaser.AUTO);
 
 game.state.add('play', {
 	init: function () {
+		// This could go into the create method too. Not sure what the benefit of one or the other is.
 		game.physics.startSystem(Phaser.Physics.ARCADE);
 	},
 
@@ -13,6 +14,24 @@ game.state.add('play', {
 	},
 
 	create: function () {
+		// Associate and add the tiles, based upon the map, to the screen.
+		this.map = this.add.tilemap('map');
+		this.map.addTilesetImage('tiles', 'tiles');
+
+		// Actually triggers the tilemap to render. Only 3 or 5 lines? Yowsa.
+		this.layer = this.map.createLayer('Tile Layer 1');
+
+		// 20 = id of the tile to collide with. Multiple ids can be passed in.
+		this.map.setCollision(20, true, this.layer);
+
+		// Add the car sprite at 48,48, and set the center to the anchor point.
+		this.car = this.add.sprite(48, 48, 'car');
+		this.car.anchor.set(0.5);
+
+		this.physics.arcade.enable(this.car);
+
+		// Enable base keyboard support.
+		this.cursors = this.input.keyboard.createCursorKeys();
 	},
 
 	update: function () {
