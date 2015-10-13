0ec52ee245066c2972a55afb41aa298e0032bed0
James Skemp <strivinglife@gmail.com>
2015-10-12 21:55:33 -0500

    Started loading necessary images.

--- Coding-Tip-5-Car/README.md
+++ Coding-Tip-5-Car/README.md
@@ -1,3 +1,5 @@
 # Coding Tip 5: Car in Grid
 
 The following is from the official site's [Phaser Coding Tips 5: How to move like Pacman Tutorial](http://phaser.io/tutorials/coding-tips-005), but updated to using the state manager and 2.4.3.
+
+Assets copyright 2015 Photon Storm Ltd.
--- Coding-Tip-5-Car/src/game.js
+++ Coding-Tip-5-Car/src/game.js
@@ -2,10 +2,14 @@
 var game = new Phaser.Game(640, 480, Phaser.AUTO);
 
 game.state.add('play', {
-	init: function() {
+	init: function () {
+		game.physics.startSystem(Phaser.Physics.ARCADE);
 	},
 
 	preload: function () {
+		game.load.tilemap('map', 'assets/maze.json', null, Phaser.Tilemap.TILED_JSON);
+		game.load.image('tiles', 'assets/tiles.png');
+		game.load.image('car', 'assets/car.png');
 	},
 
 	create: function () {
