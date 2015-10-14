fbf4cf5ce97be7520295911d180a56c98f50b15e
James Skemp <strivinglife@gmail.com>
2015-10-13 22:13:02 -0500

    Code turns, but not consistently.

--- Coding-Tip-5-Car/src/game.js
+++ Coding-Tip-5-Car/src/game.js
@@ -33,9 +33,17 @@ game.state.add('play', {
 		this.speed = 150;
 		this.turnSpeed = 150;
 
+		this.marker = new Phaser.Point();
+		this.turnPoint = new Phaser.Point();
+
+		this.roadTile = 1;
+		this.gridsize = 32;
+
+		this.directions = [null, null, null, null, null];
 		this.opposites = [Phaser.NONE, Phaser.RIGHT, Phaser.LEFT, Phaser.DOWN, Phaser.UP];
 
 		this.current = Phaser.UP;
+		this.turning = Phaser.NONE;
 
 		// Enable base keyboard support.
 		this.cursors = this.input.keyboard.createCursorKeys();
@@ -45,9 +53,43 @@ game.state.add('play', {
 
 	update: function () {
 		this.physics.arcade.collide(this.car, this.layer);
+
+		this.marker.x = this.math.snapToFloor(Math.floor(this.car.x), this.gridsize) / this.gridsize;
+		this.marker.y = this.math.snapToFloor(Math.floor(this.car.y), this.gridsize) / this.gridsize;
+
+		//  Update our grid sensors
+		this.directions[1] = this.map.getTileLeft(this.layer.index, this.marker.x, this.marker.y);
+		this.directions[2] = this.map.getTileRight(this.layer.index, this.marker.x, this.marker.y);
+		this.directions[3] = this.map.getTileAbove(this.layer.index, this.marker.x, this.marker.y);
+		this.directions[4] = this.map.getTileBelow(this.layer.index, this.marker.x, this.marker.y);
+
+		this.checkKeys();
+
+		if (this.turning !== Phaser.NONE) {
+			this.turn();
+		}
 	},
 
 	render: function () {
+	},
+
+	checkKeys: function () {
+		if (this.cursors.left.isDown && this.current !== Phaser.LEFT) {
+			this.checkDirection(Phaser.LEFT);
+		}
+		else if (this.cursors.right.isDown && this.current !== Phaser.RIGHT) {
+			this.checkDirection(Phaser.RIGHT);
+		}
+		else if (this.cursors.up.isDown && this.current !== Phaser.UP) {
+			this.checkDirection(Phaser.UP);
+		}
+		else if (this.cursors.down.isDown && this.current !== Phaser.DOWN) {
+			this.checkDirection(Phaser.DOWN);
+		}
+		else {
+			//  This forces them to hold the key down to turn the corner
+			this.turning = Phaser.NONE;
+		}
 	},
 
 	move: function (direction) {
@@ -81,6 +123,46 @@ game.state.add('play', {
 		}
 
 		return "90";
+	},
+	
+	checkDirection: function (turnTo) {
+		if (this.turning === turnTo || this.directions[turnTo] === null || this.directions[turnTo].index !== this.roadTile) {
+			//  Invalid direction if they're already set to turn that way
+			//  Or there is no tile there, or the tile isn't index a floor tile
+			return;
+		}
+
+		//  Check if they want to turn around and can
+		if (this.current === this.opposites[turnTo]) {
+			this.move(turnTo);
+		}
+		else {
+			this.turning = turnTo;
+
+			this.turnPoint.x = (this.marker.x * this.gridsize) + (this.gridsize / 2);
+			this.turnPoint.y = (this.marker.y * this.gridsize) + (this.gridsize / 2);
+		}
+	},
+
+	turn: function () {
+		var cx = Math.floor(this.car.x);
+		var cy = Math.floor(this.car.y);
+
+		//  This needs a threshold, because at high speeds you can't turn because the coordinates skip past
+		if (!this.math.fuzzyEqual(cx, this.turnPoint.x, this.threshold) || !this.math.fuzzyEqual(cy, this.turnPoint.y, this.threshold)) {
+			return false;
+		}
+
+		this.car.x = this.turnPoint.x;
+		this.car.y = this.turnPoint.y;
+
+		this.car.body.reset(this.turnPoint.x, this.turnPoint.y);
+
+		this.move(this.turning);
+
+		this.turning = Phaser.NONE;
+
+		return true;
 	}
 });
 
