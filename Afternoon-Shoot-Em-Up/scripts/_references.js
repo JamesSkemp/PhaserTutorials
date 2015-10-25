58e3ffd39c33d9e94c81f22a47dadcac7c8e10e0
James Skemp <strivinglife@gmail.com>
2015-10-25 08:07:37 -0500

    Created Visual Studio references file for Intellisense.

---
+++ Afternoon-Shoot-Em-Up/scripts/_references.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="../lib/phaser-2.4.4.js" />
+/// <reference path="../src/boot.js" />
+/// <reference path="../src/preloader.js" />
+/// <reference path="../src/mainMenu.js" />
+/// <reference path="../src/game.js" />
+/// <reference path="../src/app.js" />
