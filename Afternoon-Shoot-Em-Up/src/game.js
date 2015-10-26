9534401ce2bf528bb5833a791ae6d108eb7b2a58
James Skemp <strivinglife@gmail.com>
2015-10-26 18:36:17 -0500

    Switched game over to standard game dimension properties.

--- Afternoon-Shoot-Em-Up/src/game.js
+++ Afternoon-Shoot-Em-Up/src/game.js
@@ -60,14 +60,14 @@ BasicGame.Game.prototype = {
 	},
 
 	setupBackground: function () {
-		this.sea = this.add.tileSprite(0, 0, 800, 600, 'sea');
+		this.sea = this.add.tileSprite(0, 0, this.game.width, this.game.height, 'sea');
 		// Automatically scroll the background. Replaces the following in the update():
 		// this.sea.tilePosition.y += 0.2;
 		this.sea.autoScroll(0, 12);
 	},
 
 	setupPlayer: function () {
-		this.player = this.add.sprite(400, 550, 'player');
+		this.player = this.add.sprite(this.game.width / 2, this.game.height - 50, 'player');
 		this.player.anchor.setTo(0.5);
 		this.player.animations.add('fly', [0, 1, 2], 20, true);
 		this.player.play('fly');
@@ -133,7 +133,7 @@ BasicGame.Game.prototype = {
 	},
 
 	setupText: function () {
-		this.instructions = this.add.text(400, 500,
+		this.instructions = this.add.text(this.game.width / 2, this.game.height - 100,
 			'Use Arrow Keys to Move, Press Z to Fire\n'
 			+ 'Tapping/clicking does both',
 			{ font: '20px monospace', fill: '#fff', align: 'center' }
@@ -156,7 +156,7 @@ BasicGame.Game.prototype = {
 
 			// Spawn an enemy at the top of the screen, in a random position.
 			var enemy = this.enemyPool.getFirstExists(false);
-			enemy.reset(this.rnd.integerInRange(20, 780), 0);
+			enemy.reset(this.rnd.integerInRange(20, this.game.width - 20), 0);
 			// Randomize the speed of the enemy.
 			enemy.body.velocity.y = this.rnd.integerInRange(30, 60);
 			enemy.play('fly');
