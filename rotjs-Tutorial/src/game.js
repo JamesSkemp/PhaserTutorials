5dc0ba7f4bfac1683b5a15cd09d7f79f4c538f53
James Skemp <strivinglife@gmail.com>
2015-10-30 04:40:48 -0500

    Believe I added the functions to generate and draw a map.

--- rotjs-Tutorial/src/game.js
+++ rotjs-Tutorial/src/game.js
@@ -7,3 +7,30 @@
 		document.body.appendChild(this.display.getContainer());
 	}
 };
+
+Game.map = {};
+
+Game._generateMap = function () {
+	var digger = new ROT.Digger();
+
+	var digCallback = function (x, y, value) {
+		if (value) {
+			// Don't store walls.
+			return;
+		}
+
+		var key = x + ',' + y;
+		this.map[key] = "."
+	}
+
+	digger.create(digCallback.bind(this));
+}
+
+Game._drawWholeMap = function () {
+	for (var key in this.map) {
+		var parts = key.split(',');
+		var x = parseInt(parts[0]);
+		var y = parseInt(parts[1]);
+		this.display.draw(x, y, this.map[key]);
+	}
+}
