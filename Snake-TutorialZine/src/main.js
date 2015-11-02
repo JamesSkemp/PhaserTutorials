b8708a0a83a6ea4779ca25626df69c44a9cf8573
James Skemp <strivinglife@gmail.com>
2015-11-01 19:36:37 -0600

    Menu now loads and displays an image.

--- Snake-TutorialZine/src/main.js
+++ Snake-TutorialZine/src/main.js
@@ -1,1 +1,7 @@
-﻿
+﻿var game;
+
+game = new Phaser.Game(600, 450, Phaser.AUTO);
+
+game.state.add('Menu', Menu);
+
+game.state.start('Menu');
--- Snake-TutorialZine/src/menu.js
+++ Snake-TutorialZine/src/menu.js
@@ -1,1 +1,10 @@
-﻿
+﻿var Menu = {
+
+	preload: function () {
+		this.load.image('menu', 'assets/menu.png');
+	},
+
+	create: function () {
+		this.add.sprite(0, 0, 'menu');
+	}
+};
