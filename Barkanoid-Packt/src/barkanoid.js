0826480aa4ff8f8bbb94d45692f761c53b2dc6a4
James Skemp <strivinglife@gmail.com>
2015-11-02 10:47:17 -0600

    Preload graphics necessary for the game.

--- Barkanoid-Packt/src/barkanoid.js
+++ Barkanoid-Packt/src/barkanoid.js
@@ -1,14 +1,24 @@
-﻿var game = new Phaser.Game(800, 600, Phaser.AUTO, 'barkanoid', {
+﻿/// <reference path="../lib/phaser-2.4.4.js" />
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'barkanoid', {
 
 	preload: function () {
-		
+		game.load.path = 'assets/';
+		game.load.image('background', 'background.jpg');
+		game.load.image('tile0');
+		game.load.image('tile1');
+		game.load.image('tile2');
+		game.load.image('tile3');
+		game.load.image('tile4');
+		game.load.image('tile5');
+		game.load.image('paddle');
+		game.load.image('ball');
 	},
 
 	create: function () {
 
 	},
 
-	upldate: function () {
+	update: function () {
 		
 	}
 
