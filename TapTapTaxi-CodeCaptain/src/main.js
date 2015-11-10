1abe9f9e25e3e398c8af400747d265d6977d1c59
James Skemp <strivinglife@gmail.com>
2015-11-09 19:23:00 -0600

    Started to create game with initial states.

--- TapTapTaxi-CodeCaptain/src/main.js
+++ TapTapTaxi-CodeCaptain/src/main.js
@@ -1,1 +1,19 @@
-﻿
+﻿var GAME_WIDTH = 480;
+var GAME_HEIGHT = 640;
+
+var state = {
+	init: init,
+	preload: preload,
+	create: create,
+	update: update
+};
+
+var phaserGame = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, 'container', state);
+
+function init() { }
+
+function preload() { }
+
+function create() { }
+
+function update() { }
