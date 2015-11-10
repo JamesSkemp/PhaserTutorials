2ec9f626d94b1e3b9a3e7c069d47b983ad3abf2d
James Skemp <strivinglife@gmail.com>
2015-11-09 19:44:17 -0600

    Load road tile sprite in the center of the game.

--- TapTapTaxi-CodeCaptain/src/game/TTTGame.js
+++ TapTapTaxi-CodeCaptain/src/game/TTTGame.js
@@ -11,11 +11,15 @@ var TTTGame = (function () {
 	};
 
 	TTTGame.prototype.preload = function () {
-
+		this.game.load.path = 'assets/tiles/';
+		this.game.load.image('tile_road_1');
 	};
 
 	TTTGame.prototype.create = function () {
-
+		var sprite = this.game.add.sprite(0, 0, 'tile_road_1');
+		sprite.anchor.setTo(0.5);
+		sprite.x = this.game.world.centerX;
+		sprite.y = this.game.world.centerY;
 	};
 
 	TTTGame.prototype.update = function () {
