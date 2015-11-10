113d471fee952dd0ba79ff900438c222a34dea22
James Skemp <strivinglife@gmail.com>
2015-11-09 19:37:08 -0600

    Added Phaser.Debug plugin, but commented it out since it spikes CPU, and isn't yet needed.

--- TapTapTaxi-CodeCaptain/src/game/TTTGame.js
+++ TapTapTaxi-CodeCaptain/src/game/TTTGame.js
@@ -1,11 +1,13 @@
-﻿var TTTGame = (function () {
+﻿/// <reference path="../../lib/phaser-2.4.4.js" />
+var TTTGame = (function () {
 
 	function TTTGame(phaserGame) {
 		this.game = phaserGame;
 	}
 
 	TTTGame.prototype.init = function () {
-
+		this.game.stage.backgroundColor = '#9bd3e1';
+		//this.game.add.plugin(Phaser.Plugin.Debug);
 	};
 
 	TTTGame.prototype.preload = function () {
