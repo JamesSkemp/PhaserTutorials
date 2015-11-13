6c4750b4f7e1ec4c5b70f920b0b3d711bbfeca91
James Skemp <strivinglife@gmail.com>
2015-11-12 20:59:56 -0600

    Logo now bounces into the display.

--- PhaserTypeScript/PhaserTypeScript/app.ts
+++ PhaserTypeScript/PhaserTypeScript/app.ts
@@ -17,6 +17,10 @@
 		var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY
 			, 'logo');
 		logo.anchor.setTo(0.5);
+
+		logo.scale.setTo(0.2);
+
+		this.game.add.tween(logo.scale).to({ x: 1, y: 1 }, 2000, Phaser.Easing.Bounce.Out, true);
 	}
 }
 
