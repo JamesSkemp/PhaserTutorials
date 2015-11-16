82c2a7d44e3559e716c8b00e757d16f56ebc48b2
James Skemp <strivinglife@gmail.com>
2015-11-15 20:51:30 -0600

    Copied over tween wiggle function used for drones.

--- PhaserTypeScript/DronShooter/app.ts
+++ PhaserTypeScript/DronShooter/app.ts
@@ -19,6 +19,20 @@ class State extends Phaser.State {
 	}
 }
 
+/**
+ * Tween function to give a target a bit of wiggle, but ending back at the same spot.
+ * @param aProgress
+ * @param aPeriod1
+ * @param aPeriod2
+ * @returns
+ */
+function wiggle(aProgress: number, aPeriod1: number, aPeriod2: number): number {
+	var current1: number = aProgress * Math.PI * 2 * aPeriod1;
+	var current2: number = aProgress * Math.PI * 2 * aPeriod2;
+
+	return Math.sin(current1) * Math.cos(current2);
+}
+
 window.onload = () => {
 	new Game();
 };
