8308abc53c32f0221a4cc3601556722bc83dd9c4
James Skemp <strivinglife@gmail.com>
2015-11-15 21:15:54 -0600

    Dron now moves according to wiggle function.

--- PhaserTypeScript/DronShooter/app.ts
+++ PhaserTypeScript/DronShooter/app.ts
@@ -19,6 +19,11 @@ class State extends Phaser.State {
 		this.add.image(0, 0, "BG");
 
 		var dron: Dron = new Dron(this.game, 320, 100, "atlas", "dron1");
+
+		this.game.physics.enable(dron, Phaser.Physics.P2JS);
+
+		dron.body.kinematic = true;
+
 		dron.setUp();
 		
 		this.world.add(dron);
@@ -29,6 +34,28 @@ class Dron extends Phaser.Sprite {
 
 	public setUp() {
 		this.anchor.setTo(0.5);
+		// Put it in a random location.
+		this.reset(this.game.rnd.between(40, 600), this.game.rnd.between(60, 150));
+		// Movement range.
+		var range: number = this.game.rnd.between(60, 120);
+		// Duration of complete move.
+		var duration: number = this.game.rnd.between(30000, 50000);
+		// Random wiggle properties.
+		var xPeriod1: number = this.game.rnd.between(2, 13);
+		var xPeriod2: number = this.game.rnd.between(2, 13);
+		var yPeriod1: number = this.game.rnd.between(2, 13);
+		var yPeriod2: number = this.game.rnd.between(2, 13);
+
+		// Add tweens. Target the body since physics are enabled.
+		var xTween = this.game.add.tween(this.body);
+		xTween.to({ x: this.position.x + range }, duration, function (aProgress: number) {
+			return wiggle(aProgress, xPeriod1, xPeriod2);
+		}, true, 0, -1);
+
+		var yTween = this.game.add.tween(this.body);
+		yTween.to({ y: this.position.y + range }, duration, function (aProgress: number) {
+			return wiggle(aProgress, yPeriod1, yPeriod2);
+		}, true, 0, -1);
 	}
 }
 
