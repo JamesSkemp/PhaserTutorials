0c8986e50e89df0a9293cf76afc791c65bc22e70
James Skemp <strivinglife@gmail.com>
2015-12-02 15:59:24 -0600

    Finished part one of the tutorial.

--- RiseAbove-Phaser-Tutorial/src/game.ts
+++ RiseAbove-Phaser-Tutorial/src/game.ts
@@ -16,6 +16,9 @@ class Game extends Phaser.State {
 	shipPositions: number[];
 	shipHorizontalSpeed = 400;
 	shipMoveDelay = 500;
+	// Barriers
+	barrierGroup: Phaser.Group;
+	barrierDelay = 2000;
 
 	constructor() {
 		super();
@@ -24,12 +27,15 @@ class Game extends Phaser.State {
 	preload() {
 		this.game.load.path = 'assets/';
 		this.game.load.image('ship');
+		this.game.load.image('barrier');
 	}
 
 	create() {
 		this.shipCanMove = true;
 		this.shipPosition = 0;
 		this.shipPositions = [40, this.game.width - 40];
+
+		this.barrierGroup = this.game.add.group();
 
 		this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
@@ -41,6 +47,17 @@ class Game extends Phaser.State {
 		this.ship.body.moves = false;
 
 		this.game.input.onDown.add(this.moveShip, this);
+		this.game.time.events.loop(this.barrierDelay, () => {
+			var barrier = new Barrier(this.game, this.game.width * this.game.rnd.between(0, 1), -20);
+			this.game.add.existing(barrier);
+			this.barrierGroup.add(barrier);
+		});
+	}
+
+	update() {
+		this.game.physics.arcade.collide(this.ship, this.barrierGroup, () => {
+			this.game.state.start('Game');
+		});
 	}
 
 	moveShip(): void {
@@ -57,6 +74,24 @@ class Game extends Phaser.State {
 					this.shipCanMove = true;
 				});
 			});
+		}
+	}
+}
+
+class Barrier extends Phaser.Sprite {
+	barrierSpeed = 120;
+
+	constructor(game: Phaser.Game, x: number, y: number) {
+		super(game, x, y, "barrier");
+
+		game.physics.enable(this, Phaser.Physics.ARCADE);
+		this.anchor.set(0.5);
+	}
+
+	update() {
+		this.body.velocity.y = this.barrierSpeed;
+		if (this.y > this.game.height) {
+			this.destroy();
 		}
 	}
 }
