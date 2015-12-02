80e60398e02bc6aead6c6e25a8f7b679d2b836ce
James Skemp <strivinglife@gmail.com>
2015-12-02 13:50:20 -0600

    Game now displays a ship.

--- RiseAbove-Phaser-Tutorial/src/game.ts
+++ RiseAbove-Phaser-Tutorial/src/game.ts
@@ -2,6 +2,13 @@
 
 	game: Phaser.Game;
 
+	shipCanMove: boolean;
+	ship: Phaser.Sprite;
+	shipPosition: number;
+	shipPositions;
+	shipHorizontalSpeed = 400;
+	shipMoveDelay = 500;
+
 	constructor() {
 		this.game = new Phaser.Game(320, 480, Phaser.AUTO, 'content', {
 			preload: this.preload, create: this.create
@@ -9,9 +16,23 @@
 	}
 
 	preload() {
+		this.game.load.path = 'assets/';
+		this.game.load.image('ship');
 	}
 
 	create() {
+		this.shipCanMove = true;
+		this.shipPosition = 0;
+		this.shipPositions = [40, this.game.width - 40];
+
+		this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
+		this.ship = this.game.add.sprite(this.shipPositions[this.shipPosition], this.game.height - 40, 'ship');
+		this.ship.anchor.setTo(0.5);
+
+		this.game.physics.enable(this.ship, Phaser.Physics.ARCADE);
+		this.ship.body.allowRotation = false;
+		this.ship.body.moves = false;
 	}
 }
 
