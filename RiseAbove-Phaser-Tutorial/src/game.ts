4e69c3003add0e2fd9886c70255e0153ab3f6347
James Skemp <strivinglife@gmail.com>
2015-12-02 13:34:05 -0600

    Created basic class for the game.

--- RiseAbove-Phaser-Tutorial/src/game.ts
+++ RiseAbove-Phaser-Tutorial/src/game.ts
@@ -1,1 +1,20 @@
-﻿
+﻿class RiseAboveGame {
+
+	game: Phaser.Game;
+
+	constructor() {
+		this.game = new Phaser.Game(320, 480, Phaser.AUTO, 'content', {
+			preload: this.preload, create: this.create
+		});
+	}
+
+	preload() {
+	}
+
+	create() {
+	}
+}
+
+window.onload = () => {
+	var game = new RiseAboveGame();
+}
