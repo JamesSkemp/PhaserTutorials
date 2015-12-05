3179eecba782f2d4d57bb25558da52050f36c580
James Skemp <strivinglife@gmail.com>
2015-12-04 21:13:39 -0600

    Started on level 1 of game.

--- AdvancedPhaserTypeScriptProject/src/Game.ts
+++ AdvancedPhaserTypeScriptProject/src/Game.ts
@@ -6,6 +6,7 @@
 			this.state.add('Boot', Boot, false);
 			this.state.add('Preloader', Preloader, false);
 			this.state.add('MainMenu', MainMenu, false);
+			this.state.add('Level1', Level1, false);
 
 			this.state.start('Boot');
 		}
---
+++ AdvancedPhaserTypeScriptProject/src/Level1.ts
@@ -0,0 +1,14 @@
+﻿module Castlevania {
+	export class Level1 extends Phaser.State {
+
+		background: Phaser.Sprite;
+		music: Phaser.Sound;
+
+		create() {
+			this.background = this.add.sprite(0, 0, 'level1');
+
+			this.music = this.add.audio('music', 1, false);
+			this.music.play();
+		}
+	}
+}
