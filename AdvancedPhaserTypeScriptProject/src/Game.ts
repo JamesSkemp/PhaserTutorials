3f5900b4ff88a0a26a0215dee58cbd175f8c56b5
James Skemp <strivinglife@gmail.com>
2015-12-04 20:26:20 -0600

    Created new state for game booting.

--- AdvancedPhaserTypeScriptProject/src/Game.ts
+++ AdvancedPhaserTypeScriptProject/src/Game.ts
@@ -2,6 +2,11 @@
 	export class Game extends Phaser.Game {
 		constructor() {
 			super(800, 600, Phaser.AUTO, 'content', null);
+
+			this.state.add('Boot', Boot, false);
+			//this.state.add('Preloader', Preloader, false);
+
+			this.state.start('Boot');
 		}
 	}
 }
---
+++ AdvancedPhaserTypeScriptProject/src/boot.ts
@@ -0,0 +1,22 @@
+﻿module Castlevania {
+	export class Boot extends Phaser.State {
+		preload() {
+			this.load.image('preloadBar', 'assets/loader.png');
+		}
+
+		create() {
+			// They recommend setting this unless you need multitouch.
+			this.input.maxPointers = 1;
+
+			// Toggle whether or not the game will pause if the browser tab loses focus.
+			this.stage.disableVisibilityChange = true;
+
+			if (this.game.device.desktop) {
+				// Desktop-specific settings.
+				this.game.scale.pageAlignHorizontally = true;
+			}
+
+			this.game.state.start('Preloader', true, false);
+		}
+	}
+}
