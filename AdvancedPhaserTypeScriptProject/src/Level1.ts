8786c15bbc206bdff3c11f18e33e35d741114e45
James Skemp <strivinglife@gmail.com>
2015-12-04 21:24:33 -0600

    Add a Player to the game.

--- AdvancedPhaserTypeScriptProject/src/Level1.ts
+++ AdvancedPhaserTypeScriptProject/src/Level1.ts
@@ -3,12 +3,15 @@
 
 		background: Phaser.Sprite;
 		music: Phaser.Sound;
+		player: Castlevania.Player;
 
 		create() {
 			this.background = this.add.sprite(0, 0, 'level1');
 
 			this.music = this.add.audio('music', 1, false);
 			this.music.play();
+
+			this.player = new Player(this.game, 130, 284);
 		}
 	}
 }
