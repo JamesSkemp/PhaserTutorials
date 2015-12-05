4b96deefac1007d3f0e43be07ffea63bd52f9ad9
James Skemp <strivinglife@gmail.com>
2015-12-04 20:16:03 -0600

    Started building out the game.

---
+++ AdvancedPhaserTypeScriptProject/src/Game.ts
@@ -0,0 +1,11 @@
+﻿module Castlevania {
+	export class Game extends Phaser.Game {
+		constructor() {
+			super(800, 600, Phaser.AUTO, 'content', null);
+		}
+	}
+}
+
+window.onload = () => {
+	var game = new Castlevania.Game();
+};
