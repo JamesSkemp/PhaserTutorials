296639d38d3061aee2a1b7496b2427143c563855
James Skemp <strivinglife@gmail.com>
2015-12-05 11:06:57 -0600

    Started on an Obstacle object.

---
+++ TwoCarsPrototype-TypeScript/src/Obstacle.ts
@@ -0,0 +1,22 @@
+﻿module TwoCars {
+	export class Obstacle extends Phaser.Sprite {
+		obstacleSpeed: number = 120;
+
+		constructor(game: Phaser.Game, x: number, y: number) {
+			super(game, x, y, 'obstacle');
+
+			this.anchor.setTo(0.5);
+
+			game.physics.enable(this, Phaser.Physics.ARCADE);
+			// TODO tint
+		}
+
+		update() {
+			this.body.velocity.y = this.obstacleSpeed;
+
+			if (this.y > this.game.height) {
+				this.destroy();
+			}
+		}
+	}
+}
