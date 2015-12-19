27904982eacb59120b37bb46ebdf4bd15d8bfb1b
James Skemp <strivinglife@gmail.com>
2015-12-19 06:21:48 -0600

    Started crafting a simple starter template.

---
+++ _Starter Simple/src/Game.ts
@@ -0,0 +1,15 @@
+﻿module StarterSimpleProject {
+	export class Game extends Phaser.State {
+		preload() {
+			console.log((new Date).toISOString() + ' : Entered Game preload()');
+
+		}
+
+		create() {
+			console.log((new Date).toISOString() + ' : Entered Game create()');
+		}
+
+		update() {
+		}
+	}
+}
