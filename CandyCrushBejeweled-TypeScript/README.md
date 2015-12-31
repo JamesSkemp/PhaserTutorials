d2ad89fd4e6406495e183a813c49708058862121
James Skemp <strivinglife@gmail.com>
2015-12-31 14:15:53 -0600

    Used starter template to start project based upon Candy Crush/Bejeweled tutorial.

---
+++ CandyCrushBejeweled-TypeScript/README.md
@@ -0,0 +1,3 @@
+# Candy Crush/Bejeweled game, done in TypeScript
+
+The following is based upon Josh Morony's [How to Create a Candy Crush or Bejeweled Game in Phaser](http://www.joshmorony.com/how-to-create-a-candy-crush-or-bejeweled-game-in-phaser/), but updated to use TypeScript.
---
+++ CandyCrushBejeweled-TypeScript/src/Game.ts
@@ -0,0 +1,25 @@
+﻿module StarterProject {
+	export class Game extends Phaser.Game {
+		// Define static/constant variables here, as needed. Reference later by Game.SOME_VARIABLE.
+		//static SOME_VARIABLE: number = 10;
+
+		constructor() {
+			console.log((new Date).toISOString() + ' : Entered Game constructor()');
+
+			// Update the width (800) and height (600) accordingly.
+			super(800, 600, Phaser.AUTO, 'content');
+
+			// Add the game states.
+			this.state.add('Boot', Boot);
+			this.state.add('Preloader', Preloader);
+			this.state.add('MainMenu', MainMenu);
+
+			// Start the initial game state.
+			this.state.start('Boot');
+		}
+	}
+}
+
+window.onload = () => {
+	var game = new StarterProject.Game();
+};
