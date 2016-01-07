e33fe12c9f7b68bd5385eadedc5176c4e8881226
James Skemp <strivinglife@gmail.com>
2016-01-06 19:23:24 -0600

    Created new Play state.

--- Drag-Match-Engine-TypeScript/src/Game.ts
+++ Drag-Match-Engine-TypeScript/src/Game.ts
@@ -13,6 +13,7 @@
 			this.state.add('Boot', Boot);
 			this.state.add('Preloader', Preloader);
 			this.state.add('MainMenu', MainMenu);
+			this.state.add('Play', Play);
 
 			// Start the initial game state.
 			this.state.start('Boot');
