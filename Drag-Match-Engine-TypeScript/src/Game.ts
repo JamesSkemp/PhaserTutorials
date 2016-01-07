36970ab2341a1f5218a3405163569c17800a3b32
James Skemp <strivinglife@gmail.com>
2016-01-06 19:21:07 -0600

    Added Drag and Match asset, and actually saved project/solution.

--- Drag-Match-Engine-TypeScript/src/Game.ts
+++ Drag-Match-Engine-TypeScript/src/Game.ts
@@ -7,7 +7,7 @@
 			console.log((new Date).toISOString() + ' : Entered Game constructor()');
 
 			// Update the width (800) and height (600) accordingly.
-			super(800, 600, Phaser.AUTO, 'content');
+			super(300, 300, Phaser.AUTO, 'content');
 
 			// Add the game states.
 			this.state.add('Boot', Boot);
