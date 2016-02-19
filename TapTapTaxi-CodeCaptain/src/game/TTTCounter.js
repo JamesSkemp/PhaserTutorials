ddefd8abc35fab32985539490cef00543d700749
James Skemp <strivinglife@gmail.com>
2016-02-19 17:41:34 -0600

    Started building out new custom class for Taxi counter, based upon Sprite.

---
+++ TapTapTaxi-CodeCaptain/src/game/TTTCounter.js
@@ -0,0 +1,15 @@
+﻿var TTTCounter = (function () {
+	function TTTCounter(phaserGame, x, y) {
+		// Super class
+		Phaser.Sprite.call(this, phaserGame, x, y);
+
+		this.tween = undefined;
+		this.score = '';
+		this.game = phaserGame;
+	}
+
+	TTTCounter.prototype = Object.create(Phaser.Sprite.prototype);
+	TTTCounter.prototype.constructor = TTTCounter;
+
+	return TTTCounter;
+});
