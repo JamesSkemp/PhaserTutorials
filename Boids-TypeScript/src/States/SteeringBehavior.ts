f87fff1e4bec745332c3964289d0045351853591
James Skemp <strivinglife@gmail.com>
2016-03-13 17:42:13 -0500

    Add remaining steering behavior code.

--- Boids-TypeScript/src/States/SteeringBehavior.ts
+++ Boids-TypeScript/src/States/SteeringBehavior.ts
@@ -37,7 +37,31 @@
 		}
 
 		update() {
+			for (var i = 0; i < this.boidsAmount; i++) {
+				// Direction vector from the current boid to the target.
+				var direction = new Phaser.Point(this.target.x, this.target.y);
+				// Subtract the current boid's location.
+				direction.subtract(this.boids[i].x, this.boids[i].y);
+				// Normalize - vector length is 1 but direction remains the same.
+				direction.normalize();
+				// Set length equal to the boid's speed.
+				direction.setMagnitude(this.boids[i].speed);
+				// Subtract current velocity.
+				direction.subtract(this.boids[i].body.velocity.x, this.boids[i].body.velocity.y);
+				direction.normalize();
+				// Now set magnitude equal to force.
+				direction.setMagnitude(this.boids[i].force);
 
+				this.boids[i].body.velocity.add(direction.x, direction.y);
+				this.boids[i].body.velocity.normalize();
+				(<Phaser.Physics.Arcade.Body>(this.boids[i].body)).velocity.setMagnitude(this.boids[i].speed);
+				this.boids[i].angle = 180 + Phaser.Math.radToDeg(Phaser.Point.angle(this.boids[i].position, new Phaser.Point(this.boids[i].x + this.boids[i].body.velocity.x, this.boids[i].y + this.boids[i].body.velocity.y)));
+
+				if (this.boids[i].position.distance(this.target.position) < 2) {
+					this.target.x = this.game.rnd.between(10, this.game.width - 10);
+					this.target.y = this.game.rnd.between(10, this.game.height - 10);
+				}
+			}
 		}
 
 		paused() {
