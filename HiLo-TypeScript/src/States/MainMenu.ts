96a2ba13ca6ec3b8f346fbebf94fa7aa093bb458
James Skemp <strivinglife@gmail.com>
2016-03-19 08:41:43 -0500

    Create main menu.

--- HiLo-TypeScript/src/States/MainMenu.ts
+++ HiLo-TypeScript/src/States/MainMenu.ts
@@ -4,6 +4,15 @@
 			console.log((new Date).toISOString() + ' : Entered MainMenu create()');
 
 			// Handle user input as needed.
+			var gameTitle = this.game.add.sprite(160, 160, 'gametitle');
+			gameTitle.anchor.setTo(0.5);
+
+			var playButton = this.game.add.button(160, 320, 'play', this.playGame, this);
+			playButton.anchor.setTo(0.5);
+		}
+
+		playGame() {
+			this.game.state.start('TheGame');
 		}
 	}
 }
