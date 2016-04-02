d8d62fc369a6ab9d923d6dc9004b8ee68fdd0c7d
James Skemp <strivinglife@gmail.com>
2016-04-01 20:36:36 -0500

    Create new project for 2048 tutorial.

---
+++ TwentyFortyEight-TypeScript/README.md
@@ -0,0 +1,5 @@
+# How to create a complete HTML5 “2048” game with Phaser
+
+This project is based upon Emanuele Feronato's [How to create a complete HTML5 “2048” game with Phaser][1], but converted to TypeScript.
+
+[1]: http://www.emanueleferonato.com/2014/04/04/how-to-create-a-complete-html5-2048-game-with-phaser/
---
+++ TwentyFortyEight-TypeScript/src/States/_ExampleState.ts
@@ -0,0 +1,47 @@
+﻿module StarterProject {
+	export class ExampleState extends Phaser.State {
+		init() {
+			console.log((new Date).toISOString() + ' : Entered ExampleState init()');
+			// init can receive parameters.
+
+		}
+
+		preload() {
+			console.log((new Date).toISOString() + ' : Entered ExampleState preload()');
+
+			// Recommendation is to limit calls to the Phaser Loader only. (Interphase 1, pg 29)
+		}
+
+		loadUpdate() {
+			// Called while assets are being loaded.
+		}
+
+		create() {
+			console.log((new Date).toISOString() + ' : Entered ExampleState create()');
+
+		}
+
+		update() {
+
+		}
+
+		paused() {
+			console.log((new Date).toISOString() + ' : Entered ExampleState paused()');
+
+		}
+
+		pauseUpdate() {
+
+		}
+
+		resumed() {
+			console.log((new Date).toISOString() + ' : Entered ExampleState resumed()');
+
+		}
+
+		shutdown() {
+			console.log((new Date).toISOString() + ' : Entered ExampleState shutdown()');
+
+		}
+	}
+}
