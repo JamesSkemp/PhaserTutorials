ffe1c5894baf3c85e27749ae43ec9e3b01ca1cf6
James Skemp <strivinglife@gmail.com>
2016-04-06 20:52:51 -0500

    Add functionality to display map and ten random towns.

--- LevelSelection-TypeScript/src/States/ScrollableMap.ts
+++ LevelSelection-TypeScript/src/States/ScrollableMap.ts
@@ -1,5 +1,19 @@
 ﻿module LevelSelectProject {
 	export class ScrollableMap extends Phaser.State {
+		// Group of map and towns.
+		mapGroup: Phaser.Group;
+		// Game map.
+		map: Phaser.Image;
+		// Save start touch position.
+		startX;
+		startY;
+		// Handle multitouch.
+		moveIndex;
+		// Map scrolling speed.
+		mapSpeed = 1;
+		// Town selected.
+		candidateTown;
+
 		init() {
 			console.log((new Date).toISOString() + ' : Entered ScrollableMap init()');
 			// init can receive parameters.
@@ -21,6 +35,22 @@
 		create() {
 			console.log((new Date).toISOString() + ' : Entered ScrollableMap create()');
 
+			this.mapGroup = this.game.add.group();
+			this.map = this.game.add.image(0, 0, 'map');
+			this.mapGroup.add(this.map);
+
+			// Randomly place ten towns on the map.
+			for (var i = 0; i < 10; i++) {
+				var town = this.game.add.image(
+					this.game.rnd.between(50, this.map.width - 50)
+					, this.game.rnd.between(50, this.map.height - 50)
+					, 'town');
+				town.anchor.setTo(0.5);
+
+				town.inputEnabled = true;
+
+				this.mapGroup.add(town);
+			}
 		}
 
 		update() {
