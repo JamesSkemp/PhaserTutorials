58cc0fd0c873265cf504388fa517011f672042b6
James Skemp <strivinglife@gmail.com>
2016-04-06 22:22:31 -0500

    Add text links to view different level select options.

--- LevelSelection-TypeScript/src/States/MainMenu.ts
+++ LevelSelection-TypeScript/src/States/MainMenu.ts
@@ -4,8 +4,14 @@
 			console.log((new Date).toISOString() + ' : Entered MainMenu create()');
 
 			// Handle user input as needed.
-			//this.game.state.start('Option1');
-			this.game.state.start('ScrollableMap');
+
+			var link1 = this.game.add.text(0, 0, 'Option 1', { fill: '#fff' });
+			link1.inputEnabled = true;
+			link1.events.onInputDown.add(function () { this.game.state.start('Option1'); }, this);
+
+			var link2 = this.game.add.text(0, 50, 'Scrollable Map', { fill: '#fff' });
+			link2.inputEnabled = true;
+			link2.events.onInputDown.add(function () { this.game.state.start('ScrollableMap'); }, this);
 		}
 	}
 }
