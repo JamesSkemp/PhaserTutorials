580ee6c6985064ccceba6be45496869618f75f21
James Skemp <strivinglife@gmail.com>
2016-04-07 20:19:17 -0500

    Add functionality for scroll to keep moving, and bounce back and forth.

--- LevelSelection-TypeScript/src/States/CharacterSelection.ts
+++ LevelSelection-TypeScript/src/States/CharacterSelection.ts
@@ -10,6 +10,7 @@
 		savedPosition: Phaser.Point;
 		isBeingDragged: boolean;
 		movingSpeed: number;
+		movingAngle: number;
 
 		init() {
 			console.log((new Date).toISOString() + ' : Entered CharacterSelection init()');
@@ -55,10 +56,56 @@
 				fish.tint = this.colors[i];
 				this.scrollingMap.addChild(fish);
 			}
+
+			this.scrollingMap.events.onDragStart.add(function () {
+				this.isBeingDragged = true;
+				this.movingSpeed = 0;
+			}, this);
+			this.scrollingMap.events.onDragStop.add(function () {
+				this.isBeingDragged = false;
+			}, this);
 		}
 
 		update() {
+			var zoomed = false;
 
+			for (var i = 0; i < this.scrollingMap.children.length; i++) {
+				if (Math.abs(this.scrollingMap.children[i].worldPosition.x - this.game.width / 2) < 46 && !zoomed) {
+					this.scrollingMap.getChildAt(i).scale.set(1.5, 1.5);
+					zoomed = true;
+				} else {
+					this.scrollingMap.getChildAt(i).scale.set(1, 1);
+				}
+			}
+
+			if (this.isBeingDragged) {
+				this.savedPosition = new Phaser.Point(this.scrollingMap.x, this.scrollingMap.y);
+			} else {
+				if (this.movingSpeed > 1) {
+					this.scrollingMap.x += this.movingSpeed * Math.cos(this.movingAngle);
+
+					if (this.scrollingMap.x < this.game.width - this.scrollingMap.width) {
+						this.scrollingMap.x = this.game.width - this.scrollingMap.width;
+						this.movingSpeed *= 0.5;
+						this.movingAngle += Math.PI;
+					}
+					if (this.scrollingMap.x > 0) {
+						this.scrollingMap.x = 0;
+						this.movingSpeed *= 0.5;
+						this.movingAngle += Math.PI;
+					}
+
+					this.movingSpeed *= this.friction;
+					this.savedPosition = new Phaser.Point(this.scrollingMap.x, this.scrollingMap.y);
+				} else {
+					var distance = this.savedPosition.distance(this.scrollingMap.position);
+					var angle = this.savedPosition.angle(this.scrollingMap.position);
+					if (distance > 4) {
+						this.movingSpeed = distance;
+						this.movingAngle = angle;
+					}
+				}
+			}
 		}
 
 		paused() {
