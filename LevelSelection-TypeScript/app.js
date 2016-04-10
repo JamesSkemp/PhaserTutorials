904c9002a2af6306f3f82caf6a8de4b949bb0bde
James Skemp <strivinglife@gmail.com>
2016-04-09 20:05:24 -0500

    Update level selection example.

--- LevelSelection-TypeScript/app.js
+++ LevelSelection-TypeScript/app.js
@@ -19,6 +19,7 @@ var LevelSelectProject;
             this.state.add('Option1Game', LevelSelectProject.Option1Game);
             this.state.add('ScrollableMap', LevelSelectProject.ScrollableMap);
             this.state.add('CharacterSelection', LevelSelectProject.CharacterSelection);
+            this.state.add('DragSelection', LevelSelectProject.DragSelection);
             // Start the initial game state.
             this.state.start('Boot');
         }
@@ -235,6 +236,84 @@ var LevelSelectProject;
 })(LevelSelectProject || (LevelSelectProject = {}));
 var LevelSelectProject;
 (function (LevelSelectProject) {
+    var DragSelection = (function (_super) {
+        __extends(DragSelection, _super);
+        function DragSelection() {
+            _super.apply(this, arguments);
+            this.colors = [
+                0xac81bd, 0xff5050, 0xdab5ff, 0xb5ffda, 0xfffdd0, 0xcc0000, 0x54748b, 0x4b0082, 0x80ab2f, 0xff784e, 0xe500db, 0x223c4a, 0x223c4a, 0xf1290e, 0x648080, 0xbbc1c4, 0x6f98a2, 0x71717e
+            ];
+            this.columns = 3;
+            this.rows = 5;
+            this.thumbWidth = 60;
+            this.thumbHeight = 60;
+            this.thumbSpacing = 20;
+        }
+        DragSelection.prototype.init = function () {
+            console.log((new Date).toISOString() + ' : Entered DragSelection init()');
+            // init can receive parameters.
+        };
+        DragSelection.prototype.preload = function () {
+            console.log((new Date).toISOString() + ' : Entered DragSelection preload()');
+            // Recommendation is to limit calls to the Phaser Loader only. (Interphase 1, pg 29)
+            this.game.load.image('levelthumb');
+            this.game.load.image('transp');
+        };
+        DragSelection.prototype.create = function () {
+            console.log((new Date).toISOString() + ' : Entered DragSelection create()');
+            this.currentPage = 0;
+            this.game.stage.backgroundColor = '#004';
+            this.pageText = this.game.add.text(this.game.width / 2, 16, 'Swipe to select level page (1 / ' + this.colors.length + ')', { font: '18px Arial', fill: '#fff' });
+            this.pageText.anchor.setTo(0.5);
+            this.scrollingMap = this.game.add.tileSprite(0, 0, this.colors.length * this.game.width, this.game.height, 'transp');
+            this.scrollingMap.inputEnabled = true;
+            this.scrollingMap.input.enableDrag(false);
+            this.scrollingMap.input.allowVerticalDrag = false;
+            this.scrollingMap.input.boundsRect = new Phaser.Rectangle(this.game.width - this.scrollingMap.width, this.game.height - this.scrollingMap.height, this.scrollingMap.width * 2 - this.game.width, this.scrollingMap.height * 2 - this.game.height);
+            var rowWidth = this.thumbWidth * this.columns + this.thumbSpacing * (this.columns - 1);
+            var leftMargin = (this.game.width - rowWidth) / 2;
+            var colHeight = this.thumbHeight * this.columns + this.thumbSpacing * (this.columns - 1);
+            var topMargin = (this.game.width - colHeight) / 2;
+            for (var p = 0; p < this.colors.length; p++) {
+                for (var i = 0; i < this.columns; i++) {
+                    for (var j = 0; j < this.rows; j++) {
+                        var thumb = this.game.add.image(p * this.game.width + leftMargin + i * (this.thumbWidth + this.thumbSpacing), topMargin + j * (this.thumbHeight + this.thumbSpacing), 'levelthumb');
+                        thumb.tint = this.colors[p];
+                        this.scrollingMap.addChild(thumb);
+                    }
+                }
+            }
+            this.scrollingMap.events.onDragStart.add(function () {
+                this.startPosition = this.scrollingMap.x;
+                this.currentPosition = this.scrollingMap.x;
+            }, this);
+            this.scrollingMap.events.onDragStop.add(function () {
+                if (this.startPosition - this.scrollingMap.x > this.game.width / 8) {
+                    this.changePage(1);
+                }
+                else {
+                    if (this.startPosition - this.scrollingMap.x < -this.game.width / 8) {
+                        this.changePage(-1);
+                    }
+                    else {
+                        this.changePage(0);
+                    }
+                }
+            }, this);
+        };
+        DragSelection.prototype.changePage = function (page) {
+            this.currentPage += page;
+            this.pageText.text = 'Swipe to select level page (' + (this.currentPage + 1).toString() + ' / ' + this.colors.length + ')';
+            var tween = this.game.add.tween(this.scrollingMap).to({
+                x: this.currentPage * -this.game.width
+            }, 300, Phaser.Easing.Cubic.Out, true);
+        };
+        return DragSelection;
+    })(Phaser.State);
+    LevelSelectProject.DragSelection = DragSelection;
+})(LevelSelectProject || (LevelSelectProject = {}));
+var LevelSelectProject;
+(function (LevelSelectProject) {
     var MainMenu = (function (_super) {
         __extends(MainMenu, _super);
         function MainMenu() {
@@ -252,6 +331,9 @@ var LevelSelectProject;
             var link3 = this.game.add.text(0, 100, 'Character Selection', { fill: '#fff' });
             link3.inputEnabled = true;
             link3.events.onInputDown.add(function () { this.game.state.start('CharacterSelection'); }, this);
+            var link4 = this.game.add.text(0, 150, 'Drag Selection', { fill: '#fff' });
+            link4.inputEnabled = true;
+            link4.events.onInputDown.add(function () { this.game.state.start('DragSelection'); }, this);
         };
         return MainMenu;
     })(Phaser.State);
