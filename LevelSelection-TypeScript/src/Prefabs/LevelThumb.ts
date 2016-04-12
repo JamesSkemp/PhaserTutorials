1ed0bb57118eecd7bf99bc24418ad8e2877b0ab0
James Skemp <strivinglife@gmail.com>
2016-04-11 19:14:57 -0500

    Add new prefab for level select thumbs.

---
+++ LevelSelection-TypeScript/src/Prefabs/LevelThumb.ts
@@ -0,0 +1,9 @@
+﻿module LevelSelectProject {
+	export class LevelThumb extends Phaser.Image {
+		levelNumber: number;
+
+		constructor(game: Phaser.Game, x: number, y: number, key: string) {
+			super(game, x, y, key);
+		}
+	}
+}
